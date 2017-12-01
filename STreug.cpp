8a4843ede4afc97e7f2b20e90812b5880a2b1cfa
GregorShokolov <mrblackjops@gmail.com>
2017-12-01 16:44:31 +0600

    -

--- STreug.cpp
+++ STreug.cpp
@@ -1,23 +1,30 @@
 #include <iostream>
 #include <math.h>
-
+#include <stdio.h>
 
 STreug(int a, int b, int y)
 {
 	
-   if (a<0||b<0||y<0||y>180){
-   	std::cout << "Eror" << std::endl;
-   }
-
-   
-    
+   if (a<=0||b<=0||y<=0||y>=180){
+   	printf("Error");
+   } 
+    else{
+	
     double S = a*b*sin(y*M_PI/180)/2;
-    std::cout << "S=" << S << std::endl; 
-	   
+    printf("S= %f",S); 
+
+	      
  
     return 0;
-}
+}}
 main(){
-	std::cout<< "a=10, b=15, y=60 \n";
-	STreug(10,15,60);
+	int a,b,y;
+	scanf("%f",&a);
+	scanf("%f",&b);
+	scanf("%f",&y);
+	//std::cout<<"a= "<<a<<"b= "<<b<<"y= "<<y<<endl;
+
+	STreug(a,b,y);
 }
+
+
