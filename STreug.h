6bb4d28dddcb87415179e4c873d2a7bfaf13eeb9
GregorShokolov <34006202+GregorShokolov@users.noreply.github.com>
2017-12-05 15:26:40 +0600

    Update STreug.h

--- STreug.h
+++ STreug.h
@@ -6,20 +6,14 @@ int st(float a, float b, float y, double *S)
 	
    if (a <= 0 || b <= 0|| y <= 0 || y >= 180){
 
-int st(float a, float b, float y,double *S)
-{
-	
-   if (a<0||b<0||y<0||y>180){
 
-		return -1;
-   }
 
    
     
 
     *S = a * b * sin(y * M_PI / 180) / 2;
 
-    *S = a*b*sin(y*3.14/180)/2;
+  
 
 	   
  
